1ce937031a67e1ff25c9537b70a973e834d555fd
skyter10086 <skyter10086@aliyun.com>
2017-12-19 23:17:03 +0800

    new Person module
    根据Moose::Manual重新编写

--- Person.pm
+++ Person.pm
@@ -1,142 +1,52 @@
 package Person;
 
-use 5.24.1;
-use Moose;
-use Moose::Util::typeConstraints;
+use utf8::all; 
 use DateTime;
+use Moose;
 
-has 'name' => (
-  is => 'rw',
-  isa => 'Str',
-  required => 1,
-);
+my $People = {};
 
-has 'sex' => (
-  is => 'rw',
-  isa => enum('SEX' ,[qw(male female)]),
-  required => 1,
-);
+has 'sn' => ( is => 'ro', isa => 'Str', required => 1);
 
-has 'id' => (
-  is => 'rw',
-  isa => 'Str',
-  required => 1,
-);
+has 'name' => ( is => 'rw', isa => 'Str', required => 1);
 
-has 'scbh' => (
-  is => 'rw',
-  isa => 'Str',
-  required => 1,
-);
+has 'sex' => ( is => 'rw', isa => 'Str', required => 1);
 
-has 'aac001' => (
-  is => 'rw',
-  isa => 'Str',
-  required => 1,
-);
+has 'id' => ( is => 'rw', isa => 'Str', required => 1);
 
-has 'aab001' => (
-  is => 'rw',
-  isa => 'Str',
-  required => 1,
-);
+has 'folk' => ( is => 'rw', isa => 'Str', required => 1);
 
-has 'birth_date' => (
-  is => 'rw',
-  isa => 'DateTime',
-  required => 1,
-);
+has 'birth_date' => ( is => 'rw', isa => 'DateTime');
 
-has 'work_date' => (
-  is => 'rw',
-  isa => 'Str',
-);
+has 'phone' => (is => 'rw', isa => 'Str');
 
-has 'retire_date' => (
-  is => 'rw',
-  isa => 'Str',
-);
+has 'orgnazition' => ( is => 'rw', isa => 'Company', weak_ref => 1);
 
-has 'has_employed' => (
-  is => 'rw',
-  isa => 'Bool',
-  required => 1,
-);
+=pod
+before 'sn' => sub {
+    my ($self, $param) = @_;
+    say $param;
+    #die " You have tried to break the unique rule!\n" if $People->{$param};
+}
+=cut
 
-has 'has_insured' => (
-  is => 'rw',
-  isa => 'Bool',
-  required => 1,
-);
+sub Get_all {
+    shift;
+    return $People;
+}
 
-has 'has_retired' => (
-  is => 'rw',
-  isa => 'Bool',
-  required => 1,
-);
+sub BUILD {
+    my $self = shift;
+    
+    $People->{$self->sn} = $self;
+}
 
-has 'has_existed' => (
-  is => 'rw',
-  isa => 'Bool',
-  required => 1,
-);
-
-has 'photo_file' => (
-  is => 'rw',
-  isa => 'Str',
-);
-
-has 'DoD' => (
-  is => 'rw',
-  isa => 'DateTime',
-  );
-
-has 'folk' => (
-  is => 'rw',
-  isa => 'FOLKS',
-  required => 1,
-  );
-
-has 'region' => (
-  is => 'rw',
-  isa => 'REGIONS',
-  );
-
-has 'has_married' => (
-  is => 'rw',
-  isa => 'Bool',
-  );
-
-has 'phone' => (
-  is => 'rw',
-  isa => 'PHONE',
-  );
-
-has 'address' => (
- is => 'rw',
- isa => 'ADR',
- );
-
-has 'zipcode' => (
-  is => 'rw',
-  isa => 'ZIP',
-  );
-
-has 'si_card' => (
-  is => 'rw',
-  is =>'Str',
-  );
-
-has 'dwbm' => (
-  is => 'rw',
-  isa => 'DWBM',
-  );
+sub get {
+    my ($self, $person_sn) = @_;
+    die "You should input the sn of person!\n" unless $person_sn;
+    return $People->{$person_sn};
+}
 
 no Moose;
 
-__PACKAGE__->meta->make_immutable;
-
 1;
-
-
-
